f202b619c29aa2be227b016dce36a1639c31f93c
Kuisin <Kuisin@users.noreply.github.com>
2026-06-07 14:04:49 +0000

    deploy: 089b4e30d454303e41969de85e304fc9ce2429e2

---
+++ design-preview/assets/viewport-context-CK8oyvb4.js
@@ -0,0 +1,1 @@
+import{ap as t}from"./design-modules-Dm1pOLrC.js";const e=t.createContext("desktop"),r=e.Provider;function n(){return t.useContext(e)}function i(){return t.useContext(e)==="mobile"}export{r as ViewportProvider,i as useIsMobile,n as useViewport};
